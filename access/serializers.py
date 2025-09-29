485e403de9e344a501d546b02844be1d15ff8cbd
Franco Angeletti <franco.angeletti01@gmail.com>
2025-09-29 01:19:33 -0300

    Se actualiza el serializer para que hashee las contraseñas al crear un usuario

--- access/serializers.py
+++ access/serializers.py
@@ -5,3 +5,6 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta():
         model = Users
         fields="__all__"
+
+    def create(self, validated_data):
+        return Users.objects.create_user(**validated_data)
